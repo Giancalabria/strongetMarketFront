aa2b6d0e410c9373ffa158587b30d64ef30919b4
giancalabria <calabria.gianluca14@gmail.com>
2025-05-01 20:28:16 -0300

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { Routes, Route } from "react-router-dom";
 import { Login } from "./components/login/Login";
+import { Footer } from "./components/footer/Footer";
 
 function App() {
   return (
@@ -7,6 +8,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Login />} />
       </Routes>
+      <Footer />
     </div>
   );
 }
